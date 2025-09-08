7ebccf4fb03374700a3ce5a1db28106420bd9df3
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-08 13:16:20 +0200

    Update comment

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [sveltekit()],
   server: {
-    // Use specific host to make it work with the Keystatic frontend
-    // TODO: See if there is a way to force keystatic to use the same host as the main vite server
-    // NOTE: Related issue: https://github.com/Thinkmill/keystatic/issues/366
+    // NOTE: Keystatic redirects to `127.0.0.1` when it loads, which doesn't work with the default SvelteKit + Vite configs.
+    // Therefore, we need to make the Vite server host `127.0.0.1` to allow the server to use both localhost and 127.0.0.1
+    // Related issue: https://github.com/Thinkmill/keystatic/issues/366
     // This might be possible to remove in a preprocessing step or by patching Keystatic
+    // Ideally we should be able to could configure (or force) keystatic to use the same host as the Vite server.
     host: '127.0.0.1',
     fs: {
       allow: ['./keystatic.config.ts'],
