7c4546bb9ee455dd988f0483c7a62e372fd47a30
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-08 23:36:39 +0200

    Remove old comment

--- src/lib/keystatic/index.ts
+++ src/lib/keystatic/index.ts
@@ -3,11 +3,6 @@ import type { Handle } from '@sveltejs/kit'
 
 export { default as KeystaticCMS } from './KeystaticCMS.svelte'
 
-// IDEA: Thanks to the generic API handler, it's possible to build integrations with any Vite-based framework.
-// Keystatic could probably implement a vite plugin that allows usage with any Vite-based framework to serve a react frontend and handle incoming API requests.
-// handle requests before they are handled by the frontend/meta-frameworks.
-// IDEA: Create an issue about creating a vite plugin for Keystatic to allow usage with any Vite-based meta-framework
-
 /**
  * Create an API handler for all keystatic API requests.
  */
