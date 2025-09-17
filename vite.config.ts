2ecda3623439d973b4c9af6b6ab4c38b253c7e84
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-17 12:17:26 +0200

    Ensure GDPR compliant fonts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
+import { defineConfig, type Plugin } from 'vite'
 import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vite'
 import { keystatic } from './src/lib/keystatic'
 
+function ensureGDPRCompliantFonts(): Plugin {
+  const fontsURLRegex = /fonts\.googleapis\.com\/css2/g
+  const replacement = 'fonts.bunny.net/css'
+  return {
+    name: 'gdpr-compliant-fonts',
+    enforce: 'post',
+    transform(code) {
+      if (fontsURLRegex.test(code)) {
+        return code.replaceAll(fontsURLRegex, replacement)
+      }
+    },
+  }
+}
+
 export default defineConfig({
-  plugins: [sveltekit(), keystatic()],
+  plugins: [sveltekit(), keystatic(), ensureGDPRCompliantFonts()],
 })
