3cfb10f336891c09d375361a58b5e896a035842b
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-18 02:50:30 +0200

    Simplify the routes again since the integration handles the CMS routing automatically in the background

--- src/routes/(keystatic)/keystatic/[...rest]/_page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import { Keystatic } from '$lib/keystatic/ui'
-</script>
-
-<svelte:head>
-  <title>Keystatic CMS</title>
-</svelte:head>
-
-<Keystatic />
--- src/routes/(keystatic)/keystatic/[...rest]/_page.ts
+++
@@ -1,2 +0,0 @@
-// This is needed since Keystatic is a client side SPA, and won't render on the server
-export const ssr = false
