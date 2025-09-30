f41e86faa9306d3707d133cdaba52412120dd9ac
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-30 02:05:20 +0200

    Use separate module

--- src/lib/keystatic/index.ts
+++ src/lib/keystatic/index.ts
@@ -4,8 +4,7 @@ import { error, type Handle, type RequestEvent } from '@sveltejs/kit'
 import { cp, readdir, readFile, mkdir } from 'node:fs/promises'
 import { basename, resolve } from 'node:path'
 import { type ConfigEnv, type Plugin } from 'vite'
-import { Worker } from 'node:worker_threads'
-import { randomUUID, type UUID } from 'node:crypto'
+import { WorkerPool } from './worker-pool.ts'
 
 /**
  * Wait until a condition is true.
@@ -74,7 +73,7 @@ export async function handleKeystatic(
 
       // By serving the HTML response separately from the JS bundle, we can keep the JS cached in the browser,
       // since the same bundle is referenced by all pages. This improves performance for subsequent CMS page loads.
-      if (event.url.pathname.endsWith('js')) {
+      if (event.url.pathname.endsWith('.js')) {
         return new Response(
           await readFile(resolve(cmsOutDir, basename(event.url.pathname)), 'utf-8'),
           { headers: { 'Content-Type': 'application/javascript' } },
@@ -146,76 +145,6 @@ function getBuildMode(env: ConfigEnv): BuildMode {
 
   // Build the first time during development
   return true
-}
-
-type TaskResolver<TaskResult> = (result: TaskResult) => void
-
-type ActiveTask<TaskResult> = {
-  worker: Worker
-  resolve: TaskResolver<TaskResult>
-}
-
-class WorkerPool<
-  TaskInput,
-  TaskResult,
-  Task extends { id: UUID; data?: TaskInput } = { id: UUID; data?: TaskInput },
-> {
-  taskQueue: { task: Task; resolve: TaskResolver<TaskResult> }[] = []
-
-  #workerPath: string
-  #workers: Worker[] = []
-  #activeTasks: Map<string, ActiveTask<TaskResult>> = new Map()
-
-  constructor(workerPath: string, poolSize = 1) {
-    this.#workerPath = workerPath
-
-    for (let i = 0; i < poolSize; i++) {
-      this.addWorker()
-    }
-  }
-
-  addWorker() {
-    const worker = new Worker(this.#workerPath)
-    worker.on('message', (msg) => {
-      const { resolve } = this.#activeTasks.get(msg.id)!
-      this.#activeTasks.delete(msg.id)
-      resolve(msg.result)
-      this.checkQueue(worker)
-    })
-    worker.on('error', console.error)
-    worker.on('exit', () => {
-      this.#workers = this.#workers.filter((w) => w !== worker)
-      this.addWorker() // Replace worker if it exits unexpectedly
-    })
-    this.#workers.push(worker)
-  }
-
-  runTask(data?: TaskInput) {
-    return new Promise((resolve) => {
-      this.taskQueue.push({ task: { id: randomUUID(), data } as Task, resolve })
-      this.checkQueue()
-    })
-  }
-
-  checkQueue(workerOverride?: Worker) {
-    if (this.taskQueue.length === 0) return
-
-    const idleWorker =
-      workerOverride ||
-      this.#workers.find(
-        (worker) => ![...this.#activeTasks.values()].some((task) => task.worker === worker),
-      )
-
-    if (!idleWorker) return
-
-    const { task, resolve } = this.taskQueue.shift()!
-    this.#activeTasks.set(task.id, { worker: idleWorker, resolve })
-    idleWorker.postMessage(task)
-  }
-
-  destroy() {
-    this.#workers.forEach((worker) => worker.terminate())
-  }
 }
 
 let pool: WorkerPool<undefined, boolean>
