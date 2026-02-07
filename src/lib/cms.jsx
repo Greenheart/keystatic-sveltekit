e9ba32463cf438f84ccdd99540bdd692eaa88eea
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-02-07 06:07:46 +0100

    Fix typo

--- src/lib/cms.jsx
+++ src/lib/cms.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Keystatic } from '@keystatic/core/ui'
 import { createRoot } from 'react-dom/client'
 
-// @ts-expect-error This module is registered by the @keystatic/sveltekit Vite plugin
+// @ts-expect-error This module is registered by the keystatic-sveltekit Vite plugin
 import config from 'virtual:keystatic.config'
 
 createRoot(document.getElementById('app')).render(<Keystatic config={config} />)
