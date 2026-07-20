0b00e4d4eae5928e746a6334180f498aae5dca7f
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-07-20 22:13:35 +0200

    chore: Fix dev project environment variable definition

--- src/env.ts
+++ src/env.ts
@@ -7,5 +7,5 @@ export const variables = defineEnvVars({
   KEYSTATIC_GITHUB_CLIENT_ID: { schema: optionalString },
   KEYSTATIC_GITHUB_CLIENT_SECRET: { schema: optionalString },
   KEYSTATIC_SECRET: { schema: optionalString },
-  PUBLIC_KEYSTATIC_GITHUB_APP_SLUG: { public: true, optionalString },
+  PUBLIC_KEYSTATIC_GITHUB_APP_SLUG: { public: true, schema: optionalString },
 })
