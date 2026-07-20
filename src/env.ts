dd7c91fe3b9326d80d38e8e11d1f6836f51be956
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-07-20 21:04:06 +0200

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -7,4 +7,5 @@ export const variables = defineEnvVars({
   KEYSTATIC_GITHUB_CLIENT_ID: { schema: optionalString },
   KEYSTATIC_GITHUB_CLIENT_SECRET: { schema: optionalString },
   KEYSTATIC_SECRET: { schema: optionalString },
+  PUBLIC_KEYSTATIC_GITHUB_APP_SLUG: { public: true, optionalString },
 })
